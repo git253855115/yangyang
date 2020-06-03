c3c93f7f136e36ffd98e68aec0cb2b69e6ad514b
yangxiujiang <253855115@qq.com>
2020-06-03 23:16:11 +0800

    初始化

---
+++ yang/yang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yang
+//
+//  Created by yangxiujiang on 2020/6/3.
+//  Copyright © 2020 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yang/yang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yang
+//
+//  Created by yangxiujiang on 2020/6/3.
+//  Copyright © 2020 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
